b622baad58976a5d7f03f4cd9699de5fe8f05563
Thomas van Zelderen <103991671+TvanZelderen@users.noreply.github.com>
2024-12-03 12:22:41 +0100

    Updated ps helpers

---
+++ solutions/year2020/day06.py
@@ -0,0 +1,23 @@
+EXAMPLE_INPUT = """
+1000
+2000
+
+3000
+4000
+
+5000
+6000
+""".strip()
+
+def parse_lines(data: str):
+    return [line for line in data.splitlines()]
+
+def part1(data: str):
+    """Solution for part 1."""
+    numbers = parse_lines(data)
+    return None
+
+def part2(data: str):
+    """Solution for part 2."""
+    numbers = parse_lines(data)
+    return None 
